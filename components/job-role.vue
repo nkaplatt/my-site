cdfedd39ae597ea498035587d23a6eb46a3ed0a4
nickplatt <np326@exeter.ac.uk>
2018-09-01 16:12:32 +0100

    about page content

---
+++ components/job-role.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="job-role">
+    <div class="card">
+      <div class="job-title">{{job.title}} - <a :href="job.url" :onclick="job.ga">{{job.company}}</a></div>
+      <div class="job-description">{{job.description}}</div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    job: Object
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~/assets/scss/variables.scss';
+
+.job-role {
+  width: 100%;
+  margin: 5px 0;
+}
+
+.card {
+  background-color: $blue-contrast;
+  // color: $header-background;
+  color: $white;
+  border-radius: 5px;
+  padding: 10px 10px;
+
+  a {
+    color: $white;
+  }
+}
+
+.job-title {
+  font-size: 18px;
+  padding-bottom: 10px;
+}
+
+.job-description {
+  font-size: 15px;
+  line-height: 1.19;
+}
+
+</style>
+
--- pages/about.vue
+++ pages/about.vue
@@ -3,22 +3,57 @@
     <Header />
     <div class="hero">
       <img class="image-me" alt="Nick Platt" src="~/assets/images/nick-mobile-min.jpg"/>
-      <div class="about-me">
+      <div class="about-me-title">
         A bit <br/> about me
       </div>
     </div>
     <div class="about-content">
-      current route: {{$route.path}}
+      <div class="about-me">
+        Currently finishing my final year at Exeter University having completed a year long placement.
+        I am a full stack developer with experience in server side languages Python, Nodejs and Golang.
+        Front end experience includes React, Redux and Vuejs.
+        I've taken keen interest in DevOps methods and have experience automating workflows including automated testing strategies,
+        Jenkins and AWS.
+      </div>
+      <div class="about-title">
+        Things I've done
+      </div>
+      <JobRole v-for="job in jobs" v-bind:key="job.title" :job="job"/>
     </div>
   </section>
 </template>
 
 <script>
-import Header from '~/components/header.vue'
+import Header from '~/components/header.vue';
+import JobRole from '~/components/job-role.vue';
 
 export default {
   components: {
-    Header
+    Header,
+    JobRole
+  },
+  data: () => {
+    return {
+      jobs: [{
+        title: 'Software Developer',
+        company: 'AND Digital',
+        url: 'https://and.digital',
+        description:
+          'Worked on a variety of projects for multiple house hold name clients, roles ranged from \
+          creating and maintaining automated testing environments, front end web app building in addition \
+          too smaller projects involving natural language processing and AI bots in python.',
+        ga: "ga('send','event','External Link','Click','AND');"
+      }, {
+        title: 'Co-creator: Team Lead & Full Stack Developer',
+        company: 'LeaveorStay',
+        url: 'https://twitter.com/leaveorstayhq',
+        description:
+          'Leading a team of 5 we created a web application and wider platform to help members of the public \
+          gather (unbias) information and validate facts regarding the UK\'s EU referendum, helping them come to \
+          a conclusion of which way they wanted to vote.',
+        ga: "ga('send','event','External Link','Click','LoS');"
+      }]
+    }
   }
 }
 </script>
@@ -28,8 +63,8 @@ export default {
 
 .container {
   min-height: 100vh;
-  display: flex;
-  background-color: rgb(253, 213, 167);
+  // background-color: rgb(253, 213, 167);
+  background-color: $white;
   margin-top: 55px; 
 }
 
@@ -46,12 +81,12 @@ export default {
   margin: 60px 0 0 20px;
 }
 
-.about-me {
+.about-me-title {
   color: $white;
   font-size: 34px;
   float: right;
   text-align: center;
-  margin: 60px 45px 0 0;
+  margin: 60px 40px 0 0;
   text-shadow:
     -1px -1px 0 $black,
     1px -1px 0 $black,
@@ -65,8 +100,26 @@ export default {
 }
 
 .about-content {
-  background-color: $white;
-  color: $header-background;
+  display: flex;
+  margin: 20px 10px 50px;
+  align-items: center;
+  flex-direction: column;
+
+  @media #{$desktop-view} {
+    flex-direction: row;
+  }
 }
+
+.about-title {
+  width: 100%;
+  font-size: 28px;
+}
+
+.about-me {
+  color: $header-background;
+  padding: 0px 10px;
+  text-align: center;
+}
+
 </style>
 
